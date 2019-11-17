5f93c106d5c863345c38fa7e642d3b73b994c59b
Nat Bell <nat@natbell.com>
2019-11-17 18:46:11 -0500

    Almost got resizing the grid working

--- script.js
+++ script.js
@@ -1,12 +1,56 @@
 console.log('Hey here I am.');
 
 const container = document.querySelector('.container');
+const numCols = 30;
+const clearButton = document.createElement('button');
+clearButton.textContent = 'Clear the Screen';
+clearButton.classList.add('nav-button');
+clearButton.addEventListener('click', clearScreen);
 
-for (i=0; i<2500; i++) {
-    const thisDiv = document.createElement('div');
-    thisDiv.classList.add('square');
-    thisDiv.textContent = "X";
-    thisDiv.id = i;
-    container.appendChild(thisDiv);
+function drawBoxes(gridSize) {
+    document.documentElement.style.setProperty('--grid-size', gridSize);
+    document.documentElement.style.setProperty('--box-size', 960 / gridSize);
+    for (i=0; i<(gridSize * gridSize); i++) {
+        const thisDiv = document.createElement('div');
+        thisDiv.classList.add('square');
+        thisDiv.id = i;
+        container.appendChild(thisDiv);
+    }
 }
+
+function makeBlack(e) {
+    console.log(e.target.id);
+    let squareToChange = document.getElementById(e.target.id);
+    squareToChange.style.backgroundColor = 'black';
+    squareToChange.style.transition = 'background-color .5s';
+}
+
+function clearScreen() {
+    console.log('This will clear the screen');
+    squares.forEach((button) => {
+        button.style.backgroundColor = 'white';
+    })
+    drawNewBoxes();
+}
+
+function drawNewBoxes() {
+    newNumCols = parseInt(prompt('How many rows this time?'));
+    squares.forEach((square) => {
+        square.remove();
+    });
+    drawBoxes(newNumCols);
+    window.squares = document.querySelectorAll('.square');
+    window.squares.forEach((square) => {
+        square.addEventListener('mouseover', makeBlack);
+    });
+}
+
+drawBoxes(numCols);
+const squares = document.querySelectorAll('.square');
+squares.forEach((square) => {
+    square.addEventListener('mouseover', makeBlack);
+});
+
+document.body.appendChild(clearButton);
+
 
